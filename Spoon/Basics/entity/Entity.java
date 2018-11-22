033a2e93370152aab387769f453be3f798f3d15f
Fabian <fabian.grun@gmx.de>
2018-11-22 06:09:11 +0100

    setMethods added

--- Spoon/Basics/entity/Entity.java
+++ Spoon/Basics/entity/Entity.java
@@ -1,23 +1,28 @@
-//Basic Class for entities asdf
+//Basic Class for entities
 
 package entity;
 
 import java.util.Random;
 
-/*abstract*/ public class Entity {
-    private String 	name;			// name of entity
+/* abstract ? */ public class Entity {
+    
+	private String 	name;			// name of entity
     private int[]	attributes;		// [0] constitution [1] strength [2] dexterity [3] intelligence [4] wisdom [5] charisma
     private int		health_max;		// maximal health points
     private int		health_current;	// current health points
     private int		magic_max;		// maximal magic points
     private int		magic_current;	// current magic points
     
+//	Constructors
+
+    //	Sets: Name, Attributes, max and current Health, max and current Magic
+    
     Entity(String s , int[] a , int hm , int hc , int mm , int mc)
     {
     	name = s;
-    	attributes = new int[a.length];
+    	attributes = new int[6];
     	
-    	for(int i = 0 ; i < a.length ; i++) 
+    	for(int i = 0 ; i < 6 ; i++) 
     	{
     	attributes[i] = a[i];
     	}
@@ -28,12 +33,14 @@ import java.util.Random;
     	
     };
     
+    //	Sets: Name, Attributes, Max Health, Max Magic
+    
     Entity(String s , int[] a , int hm , int mm)
     {
     	name = s;
-    	attributes = new int[a.length];
+    	attributes = new int[6];
     	
-    	for(int i = 0 ; i < a.length ; i++) 
+    	for(int i = 0 ; i < 6 ; i++) 
     	{
     	attributes[i] = a[i];
     	}
@@ -44,50 +51,109 @@ import java.util.Random;
     	
     }
     
+//	getMethods
+    	
+    //	Name
     public String 	getName() 
-    {
+    {	
     	return name;
     }
-    
+    	
+    //	Attribute (all)
     public int[] 	getAttributes()
     {
     	return attributes;
     }
-    
+    	
+    //	Attribute Constitution
     public int 		getConstitution() 
     {
     	return attributes[0];
     }
-    
+	
+	//	Attribute Strength
     public int 		getStrength() 
     {
     	return attributes[1];
     }
     
+	//	Attribute Dexterity
     public int 		getDexterity() 
     {
     	return attributes[2];
     }
-    
+
+	//	Attribute Intelligence
     public int 		getIntelligence() 
     {
     	return attributes[3];
     }
-    
+
+	//	Attribute Wisdom
     public int 		getWisdom() 
     {
     	return attributes[4];
     }
-    
+
+	//	Attribute Charisma
     public int 		getCharisma() 
     {
     	return attributes[5];
     }
     
+//	setMethods
+    
+    //	Name
     public void		setName(String s)
     {
     	name = s;
     }
+    
+    //	All Attributes
+    public void		setAttributes(int[] a)
+    {
+    	for(int i = 0 ; i < 6 ; i++) 
+    	{
+    		attributes[i] = a[i];
+    	}
+    }
+    
+    //	Attribute Constitution
+    public void		setConstitution(int i) 
+    {
+    	attributes[0] = i;
+    }
+
+    //	Attribute Strength
+    public void		setStrength(int i) 
+    {
+    	attributes[1] = i;
+    }
+
+    //	Attribute Dexterity
+    public void		setDexterity(int i) 
+    {
+    	attributes[2] = i;
+    }
+
+    //	Attribute Intelligence
+    public void		setIntelligence(int i) 
+    {
+    	attributes[3] = i;
+    }
+
+    //	Attribute Wisdom
+    public void		setWisdom(int i) 
+    {
+    	attributes[4] = i;
+    }
+
+    //	Attribute Charisma
+    public void		setCharisma(int i) 
+    {
+    	attributes[5] = i;
+    }
+    
     //	TESTING AREA 1
     
     public static void main(String[] args)
