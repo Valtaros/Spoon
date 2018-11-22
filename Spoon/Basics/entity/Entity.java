2b516f5595cd0c120a305fca33f84cd44ad1d55a
Fabian <Fabian@Fabian-PC.fritz.box>
2018-11-22 01:03:06 +0100

    first setup 1

---
+++ Spoon/Basics/entity/Entity.java
@@ -0,0 +1,11 @@
+//Basic Class for entities
+
+package entity;
+
+public class Entity {
+    public static void main(String[] args)
+    {
+    	System.out.println("this works!");
+        return;
+    }
+}
