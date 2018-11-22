a03b6f48695036e2198e308ca7788e6590ff1e73
Fabian <Fabian@FabianLaptop>
2018-11-23 00:48:21 +0100

    st init/get/set,hp/mp/st renamed weight/size init

--- Spoon/Basics/entity/Entity.java
+++ Spoon/Basics/entity/Entity.java
@@ -6,17 +6,22 @@ import java.util.Random;
 
 /* abstract ? */ public class Entity {
     
-	private String 	name;			// name of entity
-    private int[]	attributes;		// [0] constitution [1] strength [2] dexterity [3] intelligence [4] wisdom [5] charisma
-    private int		health_max;		// maximal health points
-    private int		health_current;	// current health points
-    private int		magic_max;		// maximal magic points
-    private int		magic_current;	// current magic points
+	private String 		name;			// 	name of entity
+    private double		weight;			//	weight of entity
+    private double[]	size;			//	size of entity (height + breadth + width)
+	private int[]		attributes;		// 	[0] constitution [1] strength [2] dexterity [3] intelligence [4] wisdom [5] charisma
+//	private int[] 		resistances;	//	(p)hysical (e)lemental [0] p-blunt [1] p-piercing [2] p-slashing [3] e-fire [4] e-frost [5] e-shock [6] e-poison [7] e-light [8] e-dark 
+    private int			hp_max;			// 	maximum health points
+    private int			hp_cur;			// 	current health points
+    private int			mp_max;			// 	maximum magic points
+    private int			mp_cur;			// 	current magic points
+    private int			st_max;			//	maximum stamina
+    private int			st_cur;			//	current stamina
     
+
 //	Constructors
 
     //	Sets: Name, Attributes, max and current Health, max and current Magic
-    
     Entity(String s , int[] a , int hm , int hc , int mm , int mc)
     {
     	name = s;
@@ -26,15 +31,14 @@ import java.util.Random;
     	{
     	attributes[i] = a[i];
     	}
-    	health_max = hm;
-    	health_current = hc;
-    	magic_max = mm;
-    	magic_current = mc;
+    	hp_max = hm;
+    	hp_cur = hc;
+    	mp_max = mm;
+    	mp_cur = mc;
     	
     };
     
     //	Sets: Name, Attributes, Max Health, Max Magic
-    
     Entity(String s , int[] a , int hm , int mm)
     {
     	name = s;
@@ -44,10 +48,10 @@ import java.util.Random;
     	{
     	attributes[i] = a[i];
     	}
-    	health_max = hm;
-    	health_current = hm;
-    	magic_max = mm;
-    	magic_current = mm;
+    	hp_max = hm;
+    	hp_cur = hm;
+    	mp_max = mm;
+    	mp_cur = mm;
     	
     }
     
@@ -100,7 +104,44 @@ import java.util.Random;
     {
     	return attributes[5];
     }
-    
+
+    //	Maximum Health
+    public int 		getHealthMax() 
+    {
+    	return hp_max;
+    }
+
+    // Current Health
+    public int 		getHealthCurrent() 
+    {
+    	return hp_cur;
+    }
+
+    //	Maximum Magic
+    public int 		getMagicMax() 
+    {
+    	return mp_max;
+    }
+
+    // Current Magic
+    public int 		getMagicCurrent() 
+    {
+    	return mp_cur;
+    }
+
+    //	Maximum Stamina
+    public int 		getStaminaMax() 
+    {
+    	return st_max;
+    }
+
+    // Current Stamina
+    public int 		getStaminaCurrent() 
+    {
+    	return st_cur;
+    }
+
+        
 //	setMethods
     
     //	Name
@@ -153,6 +194,43 @@ import java.util.Random;
     {
     	attributes[5] = i;
     }
+
+    //	Maximum Health
+    public void		setHealthMax(int i) 
+    {
+    	hp_max = i;
+    }
+    
+    //	Current Health
+    public void		setHealthCurrent(int i) 
+    {
+    	hp_cur = i;
+    }
+    
+    //	Maximum Magic
+    public void		setMagicMax(int i) 
+    {
+    	mp_max = i;
+    }
+    
+    //	Current Magic
+    public void		setMagicCurrent(int i) 
+    {
+    	mp_cur = i;
+    }
+    
+    //	Maximum Stamina
+    public void		setStaminaMax(int i) 
+    {
+    	st_max = i;
+    }
+    
+    //	Current Stamina
+    public void		setStaminaCurrent(int i) 
+    {
+    	st_cur = i;
+    }
+    
     
     //	TESTING AREA 1
     
