ddf168ccc07821b8efa34fa411bb69efd6a567e1
Fabian <Fabian@FabianLaptop>
2018-11-25 22:45:56 +0100

    added Body class

---
+++ Spoon/Basics/entity/Body.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class Body {
+
+/*	
+	boolean		heads;
+	boolean		arms;
+	boolean		legs;
+	boolean		wings;
+	boolean		tails;
+	boolean		claws;
+	boolean		fins;
+	boolean		horns;
+	boolean		hands;
+*/
+	
+}
--- Spoon/Basics/entity/Race.java
+++ Spoon/Basics/entity/Race.java
@@ -1,5 +1,48 @@
+/*TODO
+-Name
+-Base Hitpoints
+-Base Magicpoints
+-Base Skills
+-Base Resistances
+-Base Attributes 
+-Size category
+-Base Spells
+-Base Knowledge
+-Base Languages
+-Base Attack Bonus
+-Base Immunities
+-Mobility
+	-CanWalk
+	-CanClimb
+	-CanFly
+	-CanSwim
+	-Speed(s)
+-Natural Body
+	-Head(s)
+	-Arm(s)
+	-Wing(s)
+	-Leg(s)
+	-Tail(s)
+	(-Tentacle(s) )
+	-Claws/Hands/Hoofs/etc.
+	-Natural Weapons
+	-Natural Armor
+	-Sizerange
+-Age
+-Diet
+*/
+
 package entity;
 
-public class Race {
+import	entity.*;
 
+public class Race {
+	private	String		name;				//	Name
+	private int			hp_base;			//	Base Hitpoints
+	private int			mp_base;			//	Base Magicpoints
+//	private Skill[]		skills_base;		//	Base Skills
+	private Resistances	resistances_base;	//	Base Resistances
+	private Attributes	attributes_base;	//	Base Attributes
+//	private int			size_category;		//	General Size Category
+	
 }
